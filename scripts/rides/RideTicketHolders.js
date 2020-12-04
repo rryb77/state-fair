70ac69b8bfa770c6b10fb37a53683bf8728f0564
rryb77 <rryb77@gmail.com>
2020-12-04 10:57:07 -0600

    Added a functional listener to track ride tickets

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,9 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", customEvent => {
+        // console.log('ticket purchased listener works!'
+        contentTarget.innerHTML += `<div class="person rider"></div>`
     })
 }
 
